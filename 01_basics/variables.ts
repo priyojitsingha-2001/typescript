d482407036c21f860043e1c6f6a73cdb97764efa
“Priyojit” <priyojitsingha01@gmail.com>
2024-07-09 19:51:20 +0530

    how to define types and type inference

--- 01_basics/variables.ts
+++ 01_basics/variables.ts
@@ -8,3 +8,15 @@ let mynum = 9;
 // mynum.toUpperCase(); // Uncommenting this will result in a TypeScript error
 
 console.log(greetings);
+
+// Number
+
+let userId: Number = 334466;
+
+// boolean
+
+let isLoggedIn: boolean=false;
+
+// type inference
+let value="Hello"; // here we don't need to use colons to define types, it is infered from the assigned value
+
